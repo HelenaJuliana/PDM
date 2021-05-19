e017093adb23946cab7af0da8c092d216d6bddf2
HelenaJuliana <helenajuliana14@gmail.com>
2021-05-18 21:29:12 -0300

    Lista de desejos

---
+++ listardesejos2/app/src/main/java/com/example/listardesejos/Desejo.kt
@@ -0,0 +1,19 @@
+package com.example.listardesejos
+
+import java.io.Serializable
+
+var id = 0
+class Desejo: Serializable {
+    var idDesejo: Int
+    var descricao: String
+    var valor: Float
+
+    constructor(descricao: String, valor: Float){
+        this.idDesejo = ++id
+        this.descricao = descricao
+        this.valor = valor
+    }
+    override fun toString(): String {
+        return "${this.descricao} - R$ ${this.valor}"
+    }
+}
---
+++ listardesejos2/app/src/main/java/com/example/listardesejos/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.listardesejos
+
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var listDesejos: ListView
+    private lateinit var btAdd: FloatingActionButton
+    private lateinit var list: ArrayList<Desejo>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.list = ArrayList()
+        this.listDesejos = findViewById(R.id.listDesejos)
+
+        this.btAdd = findViewById(R.id.fabAdd)
+        this.listDesejos.adapter = ArrayAdapter<Desejo>(this, android.R.layout.simple_list_item_1,this.list)
+
+        this.listDesejos.setOnItemLongClickListener(ClickRemove())
+
+        this.btAdd.setOnClickListener({clickAdd(it)})
+    }
+
+    fun clickAdd(view: View) {
+        val intent = Intent(this,CadastroActivity::class.java)
+
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == 1){
+                val desejo = data?.getSerializableExtra("DESEJO") as Desejo
+                (this.listDesejos.adapter as ArrayAdapter<Desejo>).add(desejo)
+                Log.i("DESEJOS", this.list.toString())
+            }
+
+        }else if (resultCode == Activity.RESULT_CANCELED){
+            Log.i("APP_TELAS", "Voltou: " + "Voltou pelo dispositivo")
+        }
+    }
+
+    inner class ClickRemove: AdapterView.OnItemLongClickListener{
+        override fun onItemLongClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long
+        ): Boolean {
+            val desejo = this@MainActivity.list[position]
+            (this@MainActivity.listDesejos.adapter as ArrayAdapter<Desejo>).remove(desejo)
+            return true
+        }
+    }
+}
