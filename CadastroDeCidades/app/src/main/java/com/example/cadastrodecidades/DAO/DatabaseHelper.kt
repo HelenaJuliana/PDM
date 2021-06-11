8b5f095e75c4e2d62329b2e56aa0abe6a309fd47
HelenaJuliana <helenajuliana14@gmail.com>
2021-06-11 19:01:08 -0300

    cidade

--- CadastroDeCidades/app/src/main/java/com/example/cadastrodecidades/DAO/DatabaseHelper.kt
+++ CadastroDeCidades/app/src/main/java/com/example/cadastrodecidades/DAO/DatabaseHelper.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DatabaseHelper  (context: Context): SQLiteOpenHelper(context, "cores.db", null, 1) {
+class DatabaseHelper  (context: Context): SQLiteOpenHelper(context, "cidade.db", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
         val sqlQuery = "" +
                 "create table cidade (" +
